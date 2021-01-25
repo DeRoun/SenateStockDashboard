4dd29c2bc2f6e0e5f3ae9a5a37ceb5e11edf468f
De'Roun Williams <deroun@uw.edu>
2021-01-24 19:47:16 -0800

    completed file creation, fixed typos, working on fixing date indexing errors

--- data_prep.py
+++ data_prep.py
@@ -13,7 +13,7 @@ s_trans = s_trans[s_trans.type != 'Exchange']
 s_trans = s_trans[s_trans.ticker != '--']
 s_trans = s_trans.drop(columns=['comment', 'ptr_link'])
 
-# add column that is 3 months before and after transaction date, boht MIN AND MAX
+# add column that is 3 months before and after transaction date, both MIN AND MAX
 
 # format transaction date data into date format
 s_trans['transaction_date']= pd.to_datetime(s_trans['transaction_date']).dt.date
@@ -29,26 +29,26 @@ s_trans['period_end'] = np.where(s_trans['transaction_date'] + three_month >= da
 
 s_trans['period_start'] = s_trans['transaction_date'] - three_month
 
-# track % points off of sale peek and purchase low
-symbols = s_trans['ticker'].tolist()
-
-
 ######################################################################################################################
 
 # create df for stock information
 stock_data = pd.DataFrame()
 
+# track % points off of sale peek and purchase low (TAKE OUT HEAD WHEN ITS FIXED)
+tick = s_trans['ticker'].head(10).tolist()
+start = pd.to_datetime('1-1-2021')
+end = datetime.date.today()
 
-####### datafram creation broken, save is fine
+####### dataframe creation broken, save is fine
 
 # loop each stock ticker into new dataframe gathering info on 3 month before and after purchase
-for i in symbols:
+for i in tick:
     try:
         # download stock data
         stock = []
-        start = pd.to_datetime(s_trans.loc[s_trans['ticker'] == symbols[i], 'period_start'].iloc[0]).dt.date
-        end = pd.to_datetime(s_trans.loc[s_trans['ticker'] == symbols[i], 'period_end'].iloc[0]).dt.date
-        stock = yf.download(i, start = start, end = end)
+        # start = pd.to_datetime(s_trans.loc[s_trans['ticker'] == tick.index(i), 'period_start'].iloc[0]).dt.date
+        # end = pd.to_datetime(s_trans.loc[s_trans['ticker'] == tick.index(i), 'period_end'].iloc[0]).dt.date
+        stock = yf.download(i, start = start, end = end, progress=False)
 
         # add each stock to dataframe
         if len(stock) == 0:
@@ -58,5 +58,14 @@ for i in symbols:
             stock_data = stock_data.append(stock, sort=False)
     except Exception:
         None
-    # download downloaded data as csv to reduce script run time
-    stock_data.to_csv('data\stock_data.csv', index=False, header=True)
+
+# set row names to first column
+stock_data.index.name = 'Date'
+stock_data.reset_index(inplace=True)
+
+#clean stock data before save
+stock_data = stock_data.drop(['Volume', 'Open', 'High', 'Low', 'Close'], axis=1)
+stock_data['Date']= pd.to_datetime(stock_data['Date']).dt.date
+
+# download downloaded data as csv to reduce script run time
+stock_data.to_csv('data\stock_data.csv', index=False, header=True)
